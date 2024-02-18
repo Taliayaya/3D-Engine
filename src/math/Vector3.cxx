51ee7fc3589263c7b2ea76f284c21544d602c31f
Taliayaya <ilanvinord@gmail.com>
2024-02-18 22:58:36 +0100

    Added base for vec3

--- src/math/Vector3.cxx
+++ src/math/Vector3.cxx
@@ -1,8 +1,27 @@
-# include "Vector3.h"
+#include "Vector3.h"
 
-extern const int MyVersion {10};
-
-int getVersion()
+namespace Math
 {
-    return 100;
-}
+
+    const Vector3 Vector3::zero{Vector3(0.0f, 0.0f, 0.0f)};
+
+    const Vector3 Vector3::up{Vector3(0.0f, 1.0f, 0.0f)};
+    const Vector3 Vector3::right{Vector3(1.0f, 0.0f, 0.0f)};
+    const Vector3 Vector3::forward{Vector3(0.0f, 0.0f, 1.0f)};
+
+    const Vector3 Vector3::down{Vector3(0.0f, -1.0f, 0.0f)};
+    const Vector3 Vector3::left{Vector3(-1.0f, 0.0f, 0.0f)};
+    const Vector3 Vector3::backward{Vector3(0.0f, 0.0f, -1.0f)};
+
+    void Vector3::Print() const
+    {
+        std::cout << "( " << x << ", " << y << ", " << z << " )\n";
+    }
+
+    std::ostream &operator<<(std::ostream &stream, Vector3 const &v)
+    {
+        stream << "( " << v.x << ", " << v.y << ", " << v.z << " )";
+        return stream;
+    }
+
+} // namespace Math
--- src/math/Vector3.h
+++ src/math/Vector3.h
@@ -1,8 +1,82 @@
-# ifndef VECTOR3_H
-# define VECTOR3_H
+#ifndef VECTOR3_H
+#define VECTOR3_H
 
-extern const int MyVersion;
+#include <cmath>
+#include <iostream>
 
-int getVersion();
+namespace Math
+{
 
-# endif
+    class Vector3
+    {
+    public:
+        float x{};
+        float y{};
+        float z{};
+
+        static const Vector3 zero;
+
+        // Right handed
+        static const Vector3 up;
+        static const Vector3 right;
+        static const Vector3 forward;
+
+        // Opposite
+        static const Vector3 down;
+        static const Vector3 left;
+        static const Vector3 backward;
+
+        constexpr Vector3(float x, float y, float z) : x{x}, y{y}, z{z}
+        {
+        }
+
+        void Print() const;
+
+        constexpr float Magnitude() const
+        {
+            return std::sqrt(x * x + y * y + z * z);
+        }
+
+        constexpr void Negate()
+        {
+            x = -x;
+            y = -y;
+            z = -z;
+        };
+
+        constexpr Vector3 Negated() const
+        {
+            return {-x, -y, -z};
+        }
+
+        constexpr void Normalize()
+        {
+            float magn = Magnitude();
+            if (magn == 0)
+                return;
+            x /= magn;
+            y /= magn;
+            z /= magn;
+        }
+
+        constexpr Vector3 Normalized() const
+        {
+            float magn = Magnitude();
+            if (magn == 0)
+                return {0.0f, 0.0f, 0.0f};
+            return {
+                x / magn,
+                y / magn,
+                z / magn};
+        }
+        friend std::ostream &operator<<(std::ostream &stream, Vector3 const &v);
+    };
+
+    static constexpr bool operator==(const Vector3 &left, const Vector3 &right)
+    {
+        return {left.x == right.x && left.y == right.y && left.z == right.z};
+    }
+
+}
+
+#endif
