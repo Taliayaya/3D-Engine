bcc295464e6e65635719e91b1517cb0a74d48a79
Ilan <ilanvinord@gmail.com>
2024-02-22 21:59:34 +0100

    setup (#3)

--- src/main.cxx
+++ src/main.cxx
@@ -1,15 +1,49 @@
 #include <iostream>
 
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+
 #include "./math/Vector3.h"
 #include "./math/Matrix4.h"
 #include "config.h"
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
 int main()
 {
-    std::cout << "Hello World: " << Engine_VERSION_MAJOR << "\n";
-    Math::Matrix4 matrix{};
-    matrix.Print();
-    Math::Matrix4 matrix2{Math::Matrix4(5)};
-    matrix2.Print();
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window{glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL)};
+    if (window == NULL)
+    {
+        std::cout << "Failed to create the GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    // GLFWframebuffersizefun
+    glfwTerminate();
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
 }
